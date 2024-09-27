c973da950b47bd574b8f3b2b9fbff82e59913a4e
Michal Gawik <gawlik.zator@o2.pl>
2024-09-27 13:41:19 +0200

    render form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import Form from './components/Form'
 
 function App() {
 	const [items, setItems] = useState(0)
 
-	return <main></main>
+	return (
+		<section>
+			<Form />
+		</section>
+	)
 }
 
 export default App
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
-	return <div></div>
+	const [item, setItem] = useState<string>('')
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		if (!item) {
+			console.log('no item')
+			return
+		}
+		console.log(123)
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input type='text' placeholder='add item' name='item' value={item} />
+			<button type='submit'>Add</button>
+		</form>
+	)
 }
 
 export default Form
