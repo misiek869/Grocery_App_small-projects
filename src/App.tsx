49152a15dd5c34835183758ba9e32a8911b6e7f2
Michal Gawik <gawlik.zator@o2.pl>
2024-09-28 05:45:19 +0200

    create logic to add item

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 import Form from './components/Form'
+import { nanoid } from 'nanoid'
 
 export type Item = {
 	name: string
 	completed: boolean
-	id: number
+	id: string
 }
 
 function App() {
 	const [items, setItems] = useState<Item[]>([])
-  
+
+	const addNewItem = (itemName: string): void => {
+		const newItem: Item = {
+			name: itemName,
+			completed: false,
+			id: nanoid(),
+		}
+		const newItems = [...items, newItem]
+		setItems(newItems)
+	}
+
 	console.log(items)
 	return (
 		<section>
-			<Form setItems={setItems} />
+			<Form addNewItem={addNewItem} />
 		</section>
 	)
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,15 +2,11 @@ import React, { useState } from 'react'
 import { Item } from '../App'
 
 type FormProps = {
-	setItems: Item[]
+	addNewItem: (itemName: string) => void
 }
 
-const Form = ({ setItems }: FormProps) => {
-	const [item, setItem] = useState<Item | undefined>({
-		name: '',
-		completed: false,
-		id: 0,
-	})
+const Form = ({ addNewItem }: FormProps) => {
+	const [item, setItem] = useState<string>('')
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
@@ -18,8 +14,8 @@ const Form = ({ setItems }: FormProps) => {
 			console.log('no item')
 			return
 		}
-		setItems(prevItems => [...prevItems, { ...item, id: prevItems.length + 1 }])
-		setItem({ name: '', completed: false, id: 0 })
+		addNewItem(item)
+		setItem('')
 	}
 
 	return (
@@ -28,8 +24,8 @@ const Form = ({ setItems }: FormProps) => {
 				type='text'
 				placeholder='add item'
 				name='item'
-				value={item.name}
-				onChange={e => setItem({ ...item, name: e.target.value })}
+				value={item}
+				onChange={e => setItem(e.target.value)}
 			/>
 			<button type='submit'>Add</button>
 		</form>
